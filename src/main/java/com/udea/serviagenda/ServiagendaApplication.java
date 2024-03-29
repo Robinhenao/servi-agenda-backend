41871e68b7c734116f1316fbd5cf708348306061
Robinson Henao <robinson.henao@udea.edu.co>
2024-03-28 22:49:49 -0500

    start project

---
+++ src/main/java/com/udea/serviagenda/ServiagendaApplication.java
@@ -0,0 +1,13 @@
+package com.udea.serviagenda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiagendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiagendaApplication.class, args);
+	}
+
+}
