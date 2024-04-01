4d53e3e2f1b5b303f45d9a4171bd1226917b1fe9
Robinson Henao <robinson.henao@udea.edu.co>
2024-03-31 23:14:36 -0500

    add security filter

--- src/main/java/com/udea/serviagenda/infra/security/SecurityConfiguration.java
+++ src/main/java/com/udea/serviagenda/infra/security/SecurityConfiguration.java
@@ -15,6 +15,11 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import static com.udea.serviagenda.dominio.user.model.Permission.*;
+import static com.udea.serviagenda.dominio.user.model.Role.ADMIN;
+import static com.udea.serviagenda.dominio.user.model.Role.EMPLOYE;
+import static org.springframework.http.HttpMethod.*;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
@@ -28,8 +33,12 @@ public class SecurityConfiguration {
                 .sessionManagement(sessionAuthenticationStrategy -> sessionAuthenticationStrategy.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(rQ -> {
                     rQ.requestMatchers("/clients/**").permitAll();
-                    rQ.requestMatchers("/employes/**").permitAll();
-                    rQ.requestMatchers("/services/**").permitAll();
+                    rQ.requestMatchers("/employes/**").hasRole(ADMIN.name());
+                    rQ.requestMatchers("/services/**").hasAnyRole(ADMIN.name(), EMPLOYE.name());
+                    rQ.requestMatchers(GET,"/services/**").hasAnyAuthority(ADMIN_READ.name(), EMPLOYE_READ.name());
+                    rQ.requestMatchers(POST,"/services/**").hasAnyAuthority(ADMIN_CREATE.name(), EMPLOYE_CREATE.name());
+                    rQ.requestMatchers(PUT,"/services/**").hasAnyAuthority(ADMIN_UPDATE.name(), EMPLOYE_UPDATE.name());
+                    rQ.requestMatchers(DELETE,"/services/**").hasAnyAuthority(ADMIN_DELETE.name(), EMPLOYE_DELETE.name());
                     rQ.requestMatchers("/auth/**").permitAll();
                     rQ.anyRequest().authenticated();
                 })
